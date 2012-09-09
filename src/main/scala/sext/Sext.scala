8f00fc83a6fdfe3a5861d27c498fe883967116af
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-10 00:53:32 +0400

    MapExtensions migration

--- src/main/scala/sext/Sext.scala
+++ src/main/scala/sext/Sext.scala
@@ -6,7 +6,14 @@ import reflect.runtime.currentMirror
 
 object Sext {
 
-  implicit def mapExtensions[K, V](x: Map[K, V]) = new MapExtensions[K, V](x)
+  implicit class MapExtensions[ K, V ](x : Map[ K, V ]) {
+    def filterValues(predicate: V => Boolean) =
+      x.filter(pair => predicate(pair._2))
+    def withValuesFilter(predicate: V => Boolean) =
+      x.withFilter(pair => predicate(pair._2))
+    def mapKeys[K2](f: K => K2) =
+      x.map(pair => f(pair._1) -> pair._2)
+  }
 
   implicit class TraversableExtensions
     [ ItemT,
