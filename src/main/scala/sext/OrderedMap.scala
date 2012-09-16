6fc2cadd9e784b9845a00b28aeb448bbfd85d1f5
Nikita Volkov <nikita.y.volkov@mail.ru>
2012-09-16 22:45:11 +0400

    ordered map fix

--- src/main/scala/sext/OrderedMap.scala
+++ src/main/scala/sext/OrderedMap.scala
@@ -4,7 +4,7 @@ import collection.immutable.Queue
 
 object OrderedMap {
   def apply[A, B](elems: (A, B)*) =
-    new OrderedMap(Map(elems: _*), Queue(elems: _*))
+    new OrderedMap(Map(elems: _*), Queue(elems.map(_._1): _*))
 }
 /**
  * An ordered map implementation that should perform effectively on all operations except
@@ -12,28 +12,28 @@ object OrderedMap {
  */
 class OrderedMap[A, B](
   map: Map[A, B],
-  protected val queue: Queue[(A, B)]
+  protected val order: Queue[A]
 ) extends Map[A, B] {
   def get(key: A) =
     map.get(key)
   def iterator =
-    queue.iterator
+    order.iterator.map(x => x -> map(x))
   def +[B1 >: B](kv: (A, B1)) =
     new OrderedMap(
       map + kv,
-      if( map.contains(kv._1) ) queue else queue enqueue kv
+      if( map.contains(kv._1) ) order else order enqueue kv._1
     )
   def -(key: A) =
     new OrderedMap(
       map - key,
-      queue filter (_._1 != key)
+      order diff Queue(key)
     )
   override def hashCode() =
-    queue.hashCode
+    order.hashCode
   override def equals(that: Any) =
     that match {
       case that: OrderedMap[A, B] =>
-        queue.equals(that.queue)
+        order.equals(that.order)
       case _ =>
         super.equals(that)
     }
