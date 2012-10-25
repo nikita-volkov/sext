84ff721b05cee68b4a6f41ef7a66ed38cafeb7d2
Nikita Volkov <justnika@mail.ru>
2012-10-25 19:26:12 +0400

    ns-based benchmarking

--- src/main/scala/sext/Benchmarking.scala
+++ src/main/scala/sext/Benchmarking.scala
@@ -1,20 +1,19 @@
 package sext
 
 import embrace._
-import org.joda.time._
 
 object Benchmarking {
 
-  def benchmark [Z] ( f : => Z ) : (Z, Period)
+  def benchmark [Z] ( f : => Z ) : (Z, Long)
     = {
-      val start = Instant.now()
+      val start = System.nanoTime()
       val result = f
-      val period = new Period(start, Instant.now())
+      val period = System.nanoTime() - start
       (result, period)
     }
   def benchmarkAndPrint [Z] ( label : String ) ( f : => Z ) : Z
     = benchmark(f) $$ {case (r, p) => println(label + ": " + p); r}
-  def benchmarkDoing [Z] ( g : Period => Unit ) ( f : => Z ) : Z
+  def benchmarkDoing [Z] ( g : Long => Unit ) ( f : => Z ) : Z
     = benchmark(f) $$ {case (r, p) => g(p); r}
 
 }
